7382b2a70ca7443c46575fd13dae05f450c28022
rajeshbbalam <rajesh@nooor.in>
2022-12-02 15:10:03 +0530

    path issues

--- flutter.js
+++ flutter.js
@@ -113,7 +113,7 @@ _flutter.loader = null;
         timeoutMillis = 4000,
       } = serviceWorkerOptions;
 
-      let serviceWorkerUrl = "flutter_service_worker.js?v=" + serviceWorkerVersion;
+      let serviceWorkerUrl = "flutter_riverpod_mutli_instance/flutter_service_worker.js?v=" + serviceWorkerVersion;
       let loader = navigator.serviceWorker.register(serviceWorkerUrl)
         .then((reg) => {
           if (!reg.active && (reg.installing || reg.waiting)) {
